812ea4e2423af2ac0349ae63d3df6fbd05aa35cd
Utkarsh Gaur <charmedtobe@gmail.com>
2017-02-15 20:47:42 -0500

    Deterministic Game
    Game works. Is too deterministic to be fun, however.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int highNumber;
+	int lowNumber;
+	int guess;
+    int maxGuessesAllowed = 10;
+    public Text numberDisplay;
+    public LevelManager myManager;
+	// Use this for initialization
+	void Start () {
+		
+		StartGame();
+	}
+	
+	void StartGame()
+	{
+        /*
+		//highNumber = highNumber + 1;
+		print ("=======================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head.");
+		*/
+		highNumber = 1001;
+		lowNumber = 1;
+        guess = 500;//Random.Range(1, 1001);
+        numberDisplay.text = "I guess you are thinking... " + guess.ToString();
+        /*
+		print("The highest number is " + highNumber);
+		print("The lowest number is " + lowNumber);
+		
+		print ("Is the number higher or lower than " + guess);
+        */      
+	}
+	
+	void NextGuess()
+	{
+        maxGuessesAllowed--;
+        if (maxGuessesAllowed <= 0)
+        {
+            myManager.LoadLevel(2);
+        }
+        
+		guess = (highNumber + lowNumber)/ 2;
+        if (maxGuessesAllowed != 1)
+        {
+            numberDisplay.text = "Gimme " + maxGuessesAllowed.ToString() +
+                " more tries. \nI guess you are thinking... " + guess.ToString();
+        }
+
+        if (maxGuessesAllowed == 1)
+        {
+            numberDisplay.text = "Gimme 1 more try. \nI guess you are thinking... " + guess.ToString();
+        }
+
+        /*
+		print ("Is the number higher or lower than " + guess);
+		print ("Up = higher, down = lower, return = equal");
+        */      
+				
+	}
+	// Update is called once per frame
+	void Update () {
+	
+        /*
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			//print ("Up arrow pressed");
+			lowNumber = guess;
+			NextGuess();
+						
+		} else if(Input.GetKeyDown(KeyCode.DownArrow)){
+			//print ("Down arrow pressed");
+			highNumber = guess;
+			NextGuess();
+						
+		} else if(Input.GetKeyDown(KeyCode.Return)){
+			print ("I won");
+			StartGame();
+		}
+        */      
+	}
+
+    public void GuessHigher()
+    {
+        lowNumber = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        highNumber = guess;
+        NextGuess();
+    }
+
+    //public void 
+}
