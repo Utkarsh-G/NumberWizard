48a2bcb208e37b0f4cdc74bf17350a57bc28f2fb
Utkarsh Gaur <charmedtobe@gmail.com>
2017-02-15 20:20:15 -0500

    First commit
    Basic scene transitions there. Nothing else.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(int sceneIndex)
+    {
+        Debug.Log("Level load requested for: " + sceneIndex);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested.");
+
+    }
+	
+}
